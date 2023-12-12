8e9de076640d35b5babb82367adbb5bc487e2f5f
Austin Hadley <admin@austinh.dev>
2023-12-12 14:11:14 -0500

    Fix Import Statement
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/js/createTable.js
+++ src/js/createTable.js
@@ -1,28 +1,38 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import {supabaseUrl, supabaseKey} from '/env/env.js';
+let createClient = window.supabase.createClient;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-const userRole = supabase.auth.user().role;
+const supabaseUrl = "http://127.0.0.1:54321";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 
-if (userRole === 'admin') {
-    document.getElementById('create-table-form').style.display = 'block';
-  } else {
-    document.getElementById('create-table-form').style.display = 'none';
+const supabase = createClient(supabaseUrl, supabaseAdminKey);
+const userRole = supabase.auth.getUser().role;
+
+if (userRole === "admin") {
+  document.getElementById("create-table-form").style.display = "block";
+} else {
+  document.getElementById("create-table-form").style.display = "none";
+}
+
+// Handle table creation form submission
+const createTableForm = document.getElementById("create-table-form");
+createTableForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const tableName = document.getElementById("table-name").value;
+
+  try {
+    await supabase.from("public").insert({
+      tablename: tableName,
+    });
+    sleep(10000);
+    alert("Table created successfully!");
+  } catch (error) {
+    alert("Error creating table:", error.message);
+    sleep(10000);
   }
-  
-  // Handle table creation form submission
-  const createTableForm = document.getElementById('create-table-form');
-  createTableForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    const tableName = document.getElementById('table-name').value;
-  
-    try {
-      await supabase.from('pg_catalog.pg_tables').insert({
-        tablename: tableName,
-      });
-      alert('Table created successfully!');
-    } catch (error) {
-      alert('Error creating table:', error.message);
-    }
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
   });
+}
