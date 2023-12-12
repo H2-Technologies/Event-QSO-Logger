3a113ede61ab066b8005bbc0f2a4d95133637735
Austin Hadley <admin@austinh.dev>
2023-12-11 23:59:09 -0500

    Add Form, Fuck Forms SAM
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/js/auth.js
+++ src/js/auth.js
@@ -5,26 +5,3 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-function collect_auth_data() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  return { email, password };
-}
-
-async function signup() {
-  const { email, password } = collect_auth_data();
-  const { user, session, error } = await supabase.auth.signUp({
-    email,
-    password,
-  });
-  console.log(user, session, error);
-}
-
-async function login() {
-  const { email, password } = collect_auth_data();
-  const { user, session, error } = await supabase.auth.signIn({
-    email,
-    password,
-  });
-  console.log(user, session, error);
-}
