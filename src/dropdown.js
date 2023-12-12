1df92eb96a0644bca4aef788e6db4de6973a3302
Deonte Palmer <ferfrink3577@gmail.com>
2023-12-11 22:30:37 -0600

    why does defer work.. someone tell me.. PLEASE

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,3 +1,13 @@
 document.getElementById("dropdownButton").addEventListener("click", function () {
     document.getElementById("dropdown").classList.toggle("hidden");
 })
+
+/*const dropdownButton = document.querySelector("#dropdownButton");
+const dropdown = document.querySelector("#dropdown");
+
+console.log(dropdownButton);
+if (dropdownButton) {
+    dropdownButton.addEventListener("click", function () {
+        dropdown.classList.toggle('hidden');
+    })
+}*/
