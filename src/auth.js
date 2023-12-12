9d2cf0c2c63568bc289ec65474f88dba0a0893ba
Austin Hadley <admin@austinh.dev>
2023-12-11 21:41:15 -0500

    Add Supabase Folder with Auth.js File
    Signed-off-by: Austin Hadley <admin@austinh.dev>

---
+++ src/auth.js
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "http://127.0.0.1:54321";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+let { data, error } = await supabase.auth.signUp({
+  email: "someone@email.com",
+  password: "YECEmBUaYnKVOIcImIwf",
+});
