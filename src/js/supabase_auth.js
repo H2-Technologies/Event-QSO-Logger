aa659312dad6e4cfb35a047f3f5e750f5225efe3
Austin Hadley <admin@austinh.dev>
2023-12-12 22:52:27 -0500

    Update to the public url's
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- src/js/supabase_auth.js
+++ src/js/supabase_auth.js
@@ -1,7 +1,7 @@
 let supabase = window.supabase;
 
-const supabaseUrl = "http://127.0.0.1:54321";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+const supabaseUrl = "https://db.dxumeuxxjloeykdfwlkz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4dW1ldXh4amxvZXlrZGZ3bGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyNTg1NzQsImV4cCI6MjAxNzgzNDU3NH0.XsGAtlYPsf4bWKrzLobL1bLTgRQnS0acNV9rPrCBw6s";
 
 async function init_supabase() {
   let client = supabase.createClient(supabaseUrl, supabaseKey);
