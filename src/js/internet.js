ccc272daf6b566d1a9925e523672ea1efc10b41f
Deonte Palmer <ferfrink3577@gmail.com>
2023-12-12 22:43:12 -0600

    No double log..SAMMMMMMM

--- src/js/internet.js
+++ src/js/internet.js
@@ -3,9 +3,9 @@ var signupButton = document.getElementById("signupButton");
 
 async function test_internet() {
   let response = await invoke("check_internet_connection");
-  console.log(response);
+  //console.log(response);
   if (response === true) {
-    console.log("HE WORKS!")
+    console.log("HE WORKS! YOU HAVE INTRANET!")
   } else {
     //signupButton.innerHTML = "Internet Detection Failed. Please fix internet and try again.";
     signupButton.removeAttribute("href");
