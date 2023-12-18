9e8f224d76a6b7bb5d8df96760f4b7d9fc6d763c
Deonte Palmer <ferfrink3577@gmail.com>
2023-12-17 23:42:54 -0600

    embedded tables

--- src/js/viewQSO.js
+++ src/js/viewQSO.js
@@ -1,6 +1,7 @@
 let callsign = "KE8YGW";
 let url = "https://qsoapi.austinh.us/qso/" + callsign;
-fetch(url, {
+let tbody = document.querySelector("tbody");
+/*fetch(url, {
     method: "GET",
     mode: "no-cors"
 })
@@ -12,4 +13,39 @@ fetch(url, {
         data[i].created_at = date.toLocaleDateString() + " " + date.toLocaleTimeString();
     }
     console.log(data);
+
 })
+*/
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();  // Use text() first to see the raw response
+    })
+    .then(text => {
+        console.log("Raw text response:", text);  // Log the raw text
+        return JSON.parse(text);  // Then try to parse it as JSON
+    })
+    .then(data => {
+        for (let i = 0; i < data.length; i++) {
+            let date = new Date(data[i].created_at);
+            data[i].created_at = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+        }
+        tbody.innerHTML = data.map(qso => {
+            return `<tr>
+            <td>${qso.band}</td>
+            <td>${qso.callsign}</td>
+            <td>${qso.exchange}</td>
+            <td>${qso.frequency}</td>
+            <td>${qso.mode}</td>
+            <td>${qso.created_at}</td>
+        </tr>`;
+        }).join("");
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+/*
+
+ */
